ee33f225f9adb6b14c2356ab6138f5f0e72cc34a
mikhail goroshkov (afterchanges) <mdgoroshkov@gmail.com>
2024-02-23 04:30:28 +0300

    implemented astar

--- Source/iron_horizon/HexTile.h
+++ Source/iron_horizon/HexTile.h
@@ -8,33 +8,35 @@
 class UStaticMeshComponent;
 
 UENUM()
-enum class HexTileType : uint8
-{
-	INVALID,
-	GRASS,
-	FOREST,
-	MOUNTAIN,
-	WATER,
-	DESERT,
-	MAX UMETA(Hidden)
+enum class HexTileType : uint8 {
+    INVALID,
+    GRASS,
+    FOREST,
+    MOUNTAIN,
+    WATER,
+    DESERT,
+    MAX UMETA(Hidden)
 };
 
 UCLASS()
-class IRON_HORIZON_API AHexTile : public AActor
-{
-	GENERATED_BODY()
+
+class IRON_HORIZON_API AHexTile : public AActor {
+    GENERATED_BODY()
 
 public:
-	UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category = "HexTile")
-	FIntPoint GridPositionIndex;
+    UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category = "HexTile")
+    FIntPoint GridPositionIndex;
+
+    UPROPERTY(VisibleInstanceOnly, BlueprintReadWrite, Category = "HexTile")
+    FIntPoint NormalizedGridPositionIndex;
 
 protected:
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "HexTile")
-	HexTileType TileType;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "HexTile")
+    HexTileType TileType;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "HexTile")
-	UStaticMeshComponent* TileMesh;
-	
-public:	
-	AHexTile();
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "HexTile")
+    UStaticMeshComponent *TileMesh;
+
+public:
+    AHexTile();
 };
